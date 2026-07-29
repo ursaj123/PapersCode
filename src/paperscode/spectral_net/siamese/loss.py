4f05610213b4efeea8a4d771726a0e591c2057c0
ursaj123 <ayush.jangid.mat20@itbhu.ac.in>
2026-07-29 23:23:19 +0530

    init

--- src/paperscode/spectral_net/siamese/loss.py
+++ src/paperscode/spectral_net/siamese/loss.py
@@ -4,7 +4,7 @@ def contrastive_loss(x1, x2, y, margin=2.0):
     # x1 -> (B, d), x2 -> (B, d), y -> (B, )
     dist = torch.norm(x1-x2, p=2, dim=1, keepdim=False) # (B, )
     return torch.mean(
-        (1-y)*dist**2 + y*(torch.maximum(0, margin-dist))**2
-    ) # (1, )
+        (1-y)*(dist**2) + y*((torch.clamp(margin-dist, min=0))**2)
+    )
 
 
